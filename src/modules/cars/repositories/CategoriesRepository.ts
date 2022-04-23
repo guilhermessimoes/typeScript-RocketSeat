986e81b29da49c9e59bba0b3df87db5c1e4c1d44
Guilherme Simoes <guilhermejunios@gmail.com>
2022-04-23 17:50:07 -0300

    Conhecendo o Singleton Pattern

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -1,14 +1,23 @@
+/* eslint-disable prettier/prettier */
 import { Category } from '../model/Category';
-import {
-    ICategoriesRepository,
-    ICreateCategoryDTO,
-} from './ICategoriesRepository';
+import { ICategoriesRepository, ICreateCategoryDTO } from './ICategoriesRepository';
 
 class CategoriesRepository implements ICategoriesRepository {
     private categories: Category[];
 
-    constructor() {
+    // eslint-disable-next-line no-use-before-define
+    private static INSTANCE: CategoriesRepository;
+
+    private constructor() {
         this.categories = [];
+    }
+
+    public static getInstance(): CategoriesRepository {
+        if (!CategoriesRepository.INSTANCE) {
+            CategoriesRepository.INSTANCE = new CategoriesRepository();
+        }
+
+        return CategoriesRepository.INSTANCE;
     }
 
     create({ name, description }: ICreateCategoryDTO): void {
