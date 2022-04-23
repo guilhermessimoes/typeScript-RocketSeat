6aaf17f583dd3efa2c45decf16fbd541ef7748a9
Guilherme Simoes <guilhermejunios@gmail.com>
2022-04-23 18:29:23 -0300

    Refatorando as rotas

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { specificationsController } from '../modules/cars/useCases/createSpecification';
 
 const specificationsRoutes = Router();
 
 specificationsRoutes.post('/', (request, response) => {
-    return createCategoryController.handle(request, response);
+    return specificationsController.handle(request, response);
 });
 
 export { specificationsRoutes };
