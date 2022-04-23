18177a22f89a681a69d6c29e1019c376c8620641
Guilherme Simoes <guilhermejunios@gmail.com>
2022-04-23 16:45:20 -0300

    Refatorante a listagem de categoria

---
+++ src/modules/cars/useCases/listCategories/ListCategoriresUseCase.ts
@@ -0,0 +1,14 @@
+import { Category } from '../../model/Category';
+import { ICategoriesRepository } from '../../repositories/ICategoriesRepository';
+
+class ListCategoriesUseCase {
+    constructor(private categoriesRepository: ICategoriesRepository) {}
+
+    execute(): Category[] {
+        const categorires = this.categoriesRepository.list();
+
+        return categorires;
+    }
+}
+
+export { ListCategoriesUseCase };
